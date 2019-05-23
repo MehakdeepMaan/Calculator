d6241dc3ff7a56c1709a8d081a87c63fae64fbb2
MehakdeepMaan <47545519+MehakdeepMaan@users.noreply.github.com>
2019-05-23 10:14:18 -0400

    Add files via upload
    here is file with unit testing which is performed on GetAddition method. all the three test  cases are passed by nUnit .

---
+++ Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AwesomeCalculator;
+using NUnit.Framework;
+
+
+namespace CalcAppTest
+
+{
+    [TestFixture]
+    class CalcTests
+    {
+        [Test]
+        public void GetAddition_Input1point4andInput2point4_Returns3point8()
+        {
+
+            //Arrange
+            double number1 = 1.4;
+            double number2 = 2.4;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void GetAddition_Input5andInput8_Returns13()
+        {
+
+            //Arrange
+            double number1 = 5;
+            double number2 = 8;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void GetAddition_Inputminus3AndInput8_Returns5()
+        {
+
+            //Arrange
+            double number1 = -3;
+            double number2 = 8;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+      
+
+
+
+
+    }
+}
